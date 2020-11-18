295ebd26bd128d9efa7bb50a91f88530007d9dd9
cortex-m4 <wx_589b497be6834dce8034dc335740e942@git.code.tencent.com>
2020-11-18 09:35:47 +0800

    添加了F1的ADC DMA

---
+++ STM32F1/HAL_LIB/ADC_DMA_HAL/MDK-ARM/RTE/_ADC_DMA_HAL/RTE_Components.h
@@ -0,0 +1,21 @@
+
+/*
+ * Auto generated Run-Time-Environment Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'ADC_DMA_HAL' 
+ * Target:  'ADC_DMA_HAL' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f10x.h"
+
+
+
+#endif /* RTE_COMPONENTS_H */
